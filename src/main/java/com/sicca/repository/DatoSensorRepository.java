65104343265aa0f24f65a3a84e3d55c835479ebf
Geris <gerardo.a.l@hotmail.com>
2025-09-30 19:15:47 -0300

    application.properties

--- src/main/java/com/sicca/repository/DatoSensorRepository.java
+++ src/main/java/com/sicca/repository/DatoSensorRepository.java
@@ -2,6 +2,8 @@ package com.sicca.repository;
 
 import com.sicca.model.iot.DatoSensorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DatoSensorRepository extends JpaRepository<DatoSensorEntity, Long> {
 }
