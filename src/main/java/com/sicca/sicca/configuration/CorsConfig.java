5e8fe2ff040e40abdbbbd3f0f81b701b4951aa11
Geris <gerardo.a.l@hotmail.com>
2025-10-02 09:13:42 -0300

    cors config

---
+++ src/main/java/com/sicca/sicca/configuration/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.sicca.sicca.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permitir todas las peticiones de todos los orígenes
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
